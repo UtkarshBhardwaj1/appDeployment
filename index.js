b3f14070edb1a4eee276127fbc0ce2cc545eaeb2
Admin <utkarsh7423@gmail.com>
2024-01-28 12:49:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express= require('express')
+const app= express()
+const port=4000
+
+app.get('/', (req, res)=>{
+
+    res.send("Hello this is server!!")
+})
+
+app.get("/facebook", (req, res)=>{
+    res.send('Utkarshdotcom')
+})
+
+app.get('/login', (req, res)=>{
+    res.send('<h1>Welcome to site</h1>')
+})
+
+app.get('/instagram', (req,res)=>{
+    res.send('<h1>Welcome to instagram!!</h1>')
+})
+
+app.listen(process.env.port, ()=>{
+    console.log(`Listening.....${port}`)
+})
